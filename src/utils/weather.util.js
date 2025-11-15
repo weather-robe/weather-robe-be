5d0be135755fb5b10c2a2e762671ffc7fa62e0e6
strfunctionk <comzun01@gmail.com>
2025-11-16 04:23:27 +0900

    chore: weather 응답 설명 추가

--- src/utils/weather.util.js
+++ src/utils/weather.util.js
@@ -1,3 +1,17 @@
+/*
+사용 가능한 값들:
+weather[0].main: 날씨 상태 (예: Clear, Clouds, Rain)
+weather[0].description: 날씨 설명 (예: 맑음, 구름 많음, 비)
+main.temp: 기온
+main.feels_like: 체감 온도
+main.temp_min: 최저 기온
+main.temp_max: 최고 기온
+main.pressure: 기압
+main.humidity: 습도
+wind.speed: 바람 속도
+wind.deg: 바람 방향
+*/
+
 import axios from "axios";
 
 export const currentWeather = async (
